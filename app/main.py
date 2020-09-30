b6ff8f638f949170f3cdc6c2b498ea2f696572e1
vertefra <verte.fra@gmail.com>
2020-09-30 01:33:02 -0400

    migration almost completed

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ from classes.user_class import User, ConnectedUsers
 
 import socketio
 import uvicorn
+import httpx
+import json
 
 fast_app = FastAPI()
 
@@ -27,7 +29,7 @@ carol_bus = "http://127.0.0.1:3005"
 
 async def send_event(endpoint: str, data: dict, event_type: str):
     event = {"type": event_type, "payload": data}
-    res = await requests.post(f"{carol_bus}/{endpoint}", json=event)
+    res = await httpx.post(f"{carol_bus}/{endpoint}", data=event)
     return res.json()
 
 # event listener from bus =====================================
@@ -37,7 +39,7 @@ async def send_event(endpoint: str, data: dict, event_type: str):
 
 @fast_app.post('/events/')
 async def event_listener(event: dict):
-    print("Carol dispatched a new event: ")
+    print("Carol dispatched a new event: ", event)
     if (event['type'] == 'userJoin'):
         user = User(event['payload'])
         if(users.connect_user(user)):
@@ -62,17 +64,56 @@ async def disconnect(sid):
     })
 
 
-@sio.on("join")
+@sio.event
 async def join(sid, data):
+    print("JOINING")
     user = User(data['user']['username'], sid=sid)
 
-    print("connection user=> ", data)
+    print("connection user=> ", data, sid)
 
     # sending event to Carol
     print('connected users ==> ', users.connected_users)
     users.connect_user(user)
     await sio.emit('joined', {
-        'connected_users': users.connected_users})
+        'connected_users': users.connected_users}, room=sid)
+
+
+@sio.on("message_to")
+async def message_to(sid, data):
+    print()
+    print("===== data after sent a message =======")
+    print(data)
+    print()
+    print("=======================================")
+    recipient_username = data['recipient_username']
+    sender_sid = data['sender_sid']
+    if(users.connected_users[recipient_username]):
+        print()
+        print("found recipient => ", recipient_username)
+        print()
+        recipient_sid = users.connected_users[recipient_username]['sid']
+        sender_username = data['sender_username']
+        timestamp = data['timestamp']
+        data['recipient_sid'] = recipient_sid
+
+        res = await send_event('events', data, 'messageSent')
+
+        print(res)
+        print("I should be after")
+
+        await emit('dispatched_message', {
+            'message': data['message'],
+            'recipient_sid': recipient_sid,
+            'sender_sid': sender_sid,
+            'recipient_username': recipient_username,
+            'sender_username': sender_username,
+            'timestamp': timestamp
+        }, room=recipient_sid)
+
+        # await emit("ok_status", {'ok_status': True})
+
+    else:
+        emit('error', {'error': 'user not identified'})
 
 
 if __name__ == "__main__":
